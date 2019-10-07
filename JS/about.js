c0d80bd0e42b6cf483f0a6cbc503d5b2f06ea438
Goorob <orobalswalqah@gmail.com>
2019-10-07 16:09:38 +0300

    aboutme

---
+++ JS/about.js
@@ -0,0 +1,52 @@
+'use strict';
+var name = prompt('What is your name ?') ;
+var music = prompt ( 'Do you feel relax when you listening to music ?')
+music = music.toUpperCase(); 
+if ( music == 'YES'|| music == 'Y'){
+    alert('Ohh nice :)')
+} else { if (music == 'NO'|| music == 'N') 
+{
+    alert ('it is fine ');
+}
+}
+console.log('music', music )
+var sport = prompt ( 'Do you playing any sport ? ')
+sport = sport.toUpperCase(); 
+if ( sport == 'YES'|| sport == 'Y'){
+    alert("It's good for your healthy :)")
+} else { if (sport== 'NO'|| sport == 'N') 
+{
+    alert ("i think it's going to be good if you try to play " );
+}
+}
+console.log('sport', sport )
+var food  = prompt ('Do you have all your meals each day ?breakfast , dinner , and lunch ?s')
+food = food.toUpperCase(); 
+if ( food == 'YES'||food == 'Y'){
+    alert(' Good for you  :)')
+} else { if (food == 'NO'|| food == 'N') 
+{
+    alert ("Please try to have it all it's good for your health  ");
+}
+}
+console.log( 'food '+food )
+var love = prompt ( 'Have you ever been inlove ?')
+love = love.toUpperCase(); 
+if ( love == 'YES'|| love == 'Y'){
+    alert ('love is life  <3')
+} else { if (love == 'NO'|| love == 'N') 
+{
+    alert ('Wish that you will oneday :) ');
+}
+}
+console.log('love', love ) ;
+var kid  = prompt ( 'Do you like kids?')
+kid = kid.toUpperCase(); 
+if (kid== 'YES'|| kid == 'Y'){
+    alert("They're cute  :D ")
+} else { if (kid == 'NO'|| kid == 'N') 
+{
+    alert (' Oh come on  :) ');
+}
+}
+console.log('kid', kid ) ;
